c33f1709d67898bdad25b56a799dc0d708d91250
Klaidas Markisius <markisius.klaidas@gmail.com>
2023-03-10 16:44:46 +0200

    Collectible items and health bar added

--- main.py
+++ main.py
@@ -24,9 +24,26 @@ grenade_thrown = False
 
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
 grenade_img = pygame.image.load('img/icons/grenade.png').convert_alpha()
+health_box_img = pygame.image.load('img/icons/health_box.png').convert_alpha()
+ammo_box_img = pygame.image.load('img/icons/ammo_box.png').convert_alpha()
+grenade_box_img = pygame.image.load('img/icons/grenade_box.png').convert_alpha()
+item_boxes = {
+    'Health': health_box_img,
+    'Ammo': ammo_box_img,
+    'Grenade': grenade_img,
+}
 
-BG = (144, 221, 120)
-RED = (255, 0, 0)
+BG = (203, 192, 180)
+RED = (208, 30, 30)
+WHITE = (255, 255, 255)
+GREEN = (92, 226, 14)
+BLACK = (0, 0, 0)
+
+font = pygame.font.SysFont('Futura', 30)
+
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 
 def draw_bg():
@@ -146,6 +163,43 @@ class Soldier(pygame.sprite.Sprite):
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
+class ItemBox(pygame.sprite.Sprite):
+    def __init__(self, item_type, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.item_type = item_type
+        self.image = item_boxes[self.item_type]
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x + TILE_SIZE / 2, y + (TILE_SIZE - self.image.get_height()))
+
+    def update(self):
+        if pygame.sprite.collide_rect(self, player):
+            if self.item_type == 'Health':
+                player.health += 25
+                if player.health > player.max_health:
+                    player.health = player.max_health
+            elif self.item_type == 'Ammo':
+                player.ammo += 15
+            elif self.item_type == 'Grenade':
+                player.grenades += 3
+            self.kill()
+
+
+class HealthBar():
+    def __init__(self, x, y, health, max_health):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.max_health = max_health
+
+    def draw(self, health):
+        self.health = health
+
+        ratio = self.health / self.max_health
+        pygame.draw.rect(screen, BLACK, (self.x - 2, self.y - 2, 154, 24))
+        pygame.draw.rect(screen, RED, (self.x, self.y, 150, 20))
+        pygame.draw.rect(screen, GREEN, (self.x, self.y, 150 * ratio, 20))
+
+
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, direction):
         pygame.sprite.Sprite.__init__(self)
@@ -243,10 +297,19 @@ enemy_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 grenade_group = pygame.sprite.Group()
 explosion_group = pygame.sprite.Group()
+item_box_group = pygame.sprite.Group()
+
+item_box = ItemBox('Health', 300, 260)
+item_box_group.add(item_box)
+item_box = ItemBox('Ammo', 500, 260)
+item_box_group.add(item_box)
+item_box = ItemBox('Grenade', 700, 260)
+item_box_group.add(item_box)
 
 player = Soldier('player', 200, 200, 3, 5, 20, 5)
-enemy = Soldier('enemy', 400, 200, 3, 5, 20, 0)
-enemy2 = Soldier('enemy', 300, 200, 3, 5, 20, 0)
+health_bar = HealthBar(10, 10, player.health, player.health)
+enemy = Soldier('enemy', 400, 245, 3, 5, 20, 0)
+enemy2 = Soldier('enemy', 300, 245, 3, 5, 20, 0)
 enemy_group.add(enemy)
 enemy_group.add(enemy2)
 
@@ -258,6 +321,15 @@ while run:
 
     draw_bg()
 
+    health_bar.draw(player.health)
+
+    draw_text('AMMO: ', font, WHITE, 10, 35)
+    for x in range(player.ammo):
+        screen.blit(bullet_img, (90 + (x * 10), 40))
+    draw_text('GRENADES: ', font, WHITE, 10, 60)
+    for x in range(player.grenades):
+        screen.blit(grenade_img, (135 + (x * 15), 62))
+
     player.update()
     player.draw()
 
@@ -268,9 +340,11 @@ while run:
     bullet_group.update()
     bullet_group.draw(screen)
     grenade_group.update()
+    item_box_group.update()
     grenade_group.draw(screen)
     explosion_group.update()
     explosion_group.draw(screen)
+    item_box_group.draw(screen)
 
     if player.alive:
         if shoot:
